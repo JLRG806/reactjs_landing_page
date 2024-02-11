3ae90a3bb5d07bad2f777b6170423a42e78e2b5e
José Luis Rodríguez <jose.rodriguez2@alumnos.uneatlantico.es>
2024-02-11 03:54:12 +0100

    feature: short modifications for tailwind testing

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,4 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
@@ -9,25 +7,32 @@ function App() {
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <h1 className="text-3xl font-bold underline">
+          Hello world!
+        </h1>
+        <table className="border-collapse border border-slate-500 ...">
+          <thead>
+            <tr>
+              <th className="border border-slate-600 ...">State</th>
+              <th className="border border-slate-600 ...">City</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td className="border border-slate-700 ...">Indiana</td>
+              <td className="border border-slate-700 ...">Indianapolis</td>
+            </tr>
+            <tr>
+              <td className="border border-slate-700 ...">Ohio</td>
+              <td className="border border-slate-700 ...">Columbus</td>
+            </tr>
+            <tr>
+              <td className="border border-slate-700 ...">Michigan</td>
+              <td className="border border-slate-700 ...">Detroit</td>
+            </tr>
+          </tbody>
+        </table>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
